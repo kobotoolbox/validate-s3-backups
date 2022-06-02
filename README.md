59d40a43df3ada7298b51c0589ffe3626488ff38
Olivier Leger <olivierleger@gmail.com>
2022-06-02 16:08:08 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,53 @@
+## Summary
+ 
+This script checks whether the latest backup file stored on S3 is valid or not.
+The validation check is on backup age but can be also on the size.
+
+## Requirements
+
+- Python 3.9+
+- pipenv
+
+## Installation
+
+Using `pipenv install`
+
+
+## Usage
+
+#### To launch the script and the HTTP server
+
+`pipenv run python run.py`
+
+1. Options
+
+`run.py` accepts two options:
+
+- `--port` The port the HTTP server should listen to. Default is `9090`
+- `--config` The path of the configuration file the script should look for. Default is `./config.json`
+
+
+#### To access the result of the backup check
+
+With a browser, go to `http://localhost:9090/<environment>/<backup>/`
+
+If access is protected, add `?token=<token>` at the end the URL where `<token>` is the token provided in the `token` property of the config file.
+
+
+## Configuration file
+
+A configuration file sample is provided and can be used as a starter.
+
+The only required setting is `age`. By default, the value is in hours. But it understands minutes, hours, days and weeks when the unit is specified. For example: `90M` (90 minutes),`3H` (3 hours), `2D` (2 days) or `3W` (3 weeks).
+
+The three settings below are optional, but it is recommended to set them to narrow down the search of the correct backup file on the S3 bucket:
+
+- `prefix` The beginning of the backup file path. Default is `/` (the whole bucket).
+- `suffix` The end of the backup file. It can be the extension, but can be something longer. Default is `None`.
+- `min_size` The minimum size in kB the backup file should weight to be considered as a valid backup. Default is `None`.
+
+## Secure the endpoint
+
+A token can be used to secure the endpoint. In a configuration file, just add `token` property at the root level on the JSON.
+From the browser, go to `http://localhost:9090/<environment>/<backup>/?token=<token>`.
+_Notes: if you try to access the endpoint with a token but no token are set in the config, it will raise a 500 error'_
---
+++ run.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+import argparse
+import http.server
+import json
+import os
+import string
+from datetime import datetime
+from functools import partial
+from http import HTTPStatus
+from typing import Dict
+from zoneinfo import ZoneInfo
+from urllib.parse import parse_qs, parse_qsl
+
+import boto3
+
+
+config_path = './config.json'
+
+
+class BackupConfigNotFoundException(Exception):
+    pass
+
+
+class BackupFileNotFoundException(Exception):
+    pass
+
+
+class BadSizeFileException(Exception):
+    pass
+
+
+class ImproperlyConfiguredBackupConfigException(Exception):
+    pass
+
+
+class TooOldBackupException(Exception):
+    pass
+
+
+class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def __init__(self, config_path, *args, **kwargs):
+        self._config_path = config_path
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        path = self.path.strip('/')
+
+        try:
+            environment, service = path.split('/')
+        except ValueError:
+            self._create_response(
+                content='Please specify an environment and service',
+                status_code=HTTPStatus.BAD_REQUEST,
+            )
+            return
+
+        try:
+            global_config = self._get_config()
+        except FileNotFoundError:
+            self._create_response(
+                content='Could not find config file',
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+            )
+            return
+        except json.decoder.JSONDecodeError:
+            self._create_response(
+                content='Could not decode config file',
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+            )
+            return
+
+        # If endpoint is protected, validate token is provided.
+        if token := global_config.get('token'):
+            if '?' in service:
+                service, qs = service.split('?')
+                parsed_qs = parse_qs(qs)
+                # `parse_qs()` returns an array.
+                qs_token = ''.join(parsed_qs.get('token', []))
+                if qs_token != token:
+                    self._create_response(
+                        content='Access forbidden',
+                        status_code=HTTPStatus.FORBIDDEN,
+                    )
+                    return
+
+        try:
+            environment_config = global_config[environment]
+        except KeyError:
+            status_code = HTTPStatus.NOT_FOUND
+            output = 'Could not find environment'
+        else:
+            status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+            try:
+                self._validate_backup(environment_config, service)
+            except ImproperlyConfiguredBackupConfigException:
+                output = 'Backup configuration is improperly configured'
+            except BackupConfigNotFoundException:
+                output = f'Could not find `{service}` backup settings'
+                status_code = HTTPStatus.BAD_REQUEST
+            except BackupFileNotFoundException:
+                output = (
+                    f'Could not find any files matching `{service}` backup settings'
+                )
+                status_code = HTTPStatus.NOT_FOUND
+
+            except BadSizeFileException:
+                output = f'Latest backup found is too small'
+            except TooOldBackupException:
+                output = f'Latest backup found is too old'
+            else:
+                # Everything is ok, let's revert the status code to 200
+                status_code = HTTPStatus.OK
+                output = 'Backup is OK!'
+
+        self._create_response(content=output, status_code=status_code)
+
+    def _convert_age_to_seconds(self, age: str) -> int:
+        """
+        Convert expression to corresponding time in seconds.
+
+        Support minutes, hours, days, weeks
+        E.g: "1D" -> 86400
+
+        If no units are provided, it falls back on hours
+        """
+        valid_units = {
+            'M': 60,
+            'H': 60 * 60,
+            'D': 60 * 60 * 24,
+            'W': 60 * 60 * 24 * 7,
+        }
+        unit = str(age)[-1]
+        if unit not in string.ascii_letters:
+            unit = 'H'
+        else:
+            age = age[0:-1]
+
+        if unit not in valid_units.keys():
+            raise ImproperlyConfiguredBackupConfigException
+
+        return int(age) * valid_units[unit]
+
+    def _create_response(
+        self,
+        content: str,
+        status_code: HTTPStatus,
+        content_type: str = 'text/plain; charset=UTF-8',
+    ):
+        self.send_response(status_code)
+        self.send_header('Content-Type', content_type)
+        self.end_headers()
+        self.wfile.write(content.encode('utf-8'))
+
+    def _get_config(self) -> Dict:
+        with open(self._config_path, 'r') as f:
+            config = f.read()
+
+        return json.loads(config)
+
+    def _validate_backup(self, config: Dict, backup: str):
+        bucket_name = config['bucket_name']
+        access_key = config['access_key']
+        secret = config['secret_key']
+        region = config.get('region')
+
+        try:
+            backup_config = config['backups'][backup]
+        except KeyError:
+            raise BackupConfigNotFoundException
+
+        # required settings
+        try:
+            age = backup_config['age']
+        except KeyError:
+            raise ImproperlyConfiguredBackupConfigException
+
+        prefix = backup_config.get('prefix', '/')
+        suffix = backup_config.get('suffix')
+        min_size = backup_config.get('min_size')
+
+        # Ensure prefix endswith trailing slash
+        if not prefix.endswith('/'):
+            prefix = f'{prefix}/'
+
+        s3 = boto3.resource(
+            's3',
+            region_name=region,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret,
+        )
+        bucket = s3.Bucket(bucket_name)
+        last_backup = None
+        get_last_modified = lambda o: int(o.last_modified.strftime('%s'))
+        # Notice: AWS API only returns 1000 objets max.
+        # See https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html
+        obj_summaries = bucket.objects.filter(Prefix=prefix)
+        # Unfortunately, there is no way to get results sorted from latest to
+        # oldest. We need to sort in Python. Because the API returns only 1000
+        # items max, there could be a chance that the backup file we are looking
+        # for, is not part of `obj_summaries` (i.e.: when more than 1000 files
+        # match search criteria).
+        obj_summaries = sorted(obj_summaries, key=get_last_modified, reverse=True)
+        if suffix:
+            for obj_summary in obj_summaries:
+                if obj_summary.key.endswith(suffix):
+                    last_backup = obj_summary
+                    break
+        elif obj_summaries:
+            last_backup = list(obj_summaries)[0]
+
+        if not last_backup:
+            raise BackupFileNotFoundException
+
+        print(' int(last_backup.size / 1024)',  int(last_backup.size / 1024), flush=True)
+        if min_size and min_size > int(last_backup.size / 1024):
+            raise BadSizeFileException
+
+        now = datetime.now(tz=ZoneInfo('UTC'))
+        diff = now - last_backup.last_modified
+        if diff.total_seconds() > self._convert_age_to_seconds(age):
+            raise TooOldBackupException
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p',
+        '--port',
+        required=False,
+        help='Listen to this port to run the HTTP server',
+        default='9090',
+        type=int,
+    )
+
+    parser.add_argument(
+        '-c',
+        '--config',
+        required=False,
+        help='Path to config.json',
+        default=os.path.realpath(os.path.join(
+            os.path.dirname(__file__),
+            'config.json',
+        ))
+    )
+
+    args = parser.parse_args()
+
+    handler = partial(MyHTTPRequestHandler, config_path)
+    with http.server.ThreadingHTTPServer(
+            ('', args.port), handler
+    ) as httpd:
+        print(f'Ready for service on port {args.port}.')
+        httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
